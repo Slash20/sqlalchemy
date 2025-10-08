031909b196b795336cdf4d6beacbd5017bfe5f46
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 12:59:35 +0200

    add orm

--- src/models.py
+++ src/models.py
@@ -1,4 +1,18 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+
+
+
+class WorkersORM(Base):
+    __tablename__ = "workers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+
+
+
 
 metadata_obj = MetaData()
 
@@ -6,5 +20,5 @@ workers_table = Table(
     "workers",
     metadata_obj,
     Column("id", Integer, primary_key=True),
-    Column("username", String, primary_key=True),
+    Column("username", String),
 )

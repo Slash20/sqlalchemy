cd9e6815a2add7b772bd279db6291a5decb8e022
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 15:49:13 +0200

    del spaces

--- src/models.py
+++ src/models.py
@@ -3,15 +3,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 
-
-
 class WorkersORM(Base):
     __tablename__ = "workers"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
-
-
 
 
 metadata_obj = MetaData()

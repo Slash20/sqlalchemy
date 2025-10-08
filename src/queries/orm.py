95d37545369d4c3cf9b3bed5acf6a45c81d910aa
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 12:59:55 +0200

    add orm

---
+++ src/queries/orm.py
@@ -0,0 +1,18 @@
+from database import async_engine, sync_engine, session_factory
+from sqlalchemy import text, insert
+from models import metadata_obj, WorkersORM
+
+
+def create_tables():
+    sync_engine.echo = False
+    metadata_obj.drop_all(sync_engine)
+    metadata_obj.create_all(sync_engine)
+    sync_engine.echo = True
+
+
+def insert_data():
+    worker_bobr = WorkersORM(username="Bobr")
+    worker_wolf = WorkersORM(username="Wolf")
+    with session_factory() as session:
+        session.add_all([worker_bobr, worker_wolf])
+        session.commit()

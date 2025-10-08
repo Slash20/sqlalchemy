31c9e8bd781254b5c2b5abd7f8a6d10923203dcc
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 12:58:33 +0200

    add insert

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,7 +1,6 @@
 from database import async_engine, sync_engine
-from sqlalchemy import text
-from models import metadata_obj
-
+from sqlalchemy import text, insert
+from models import metadata_obj, workers_table
 
 # async def async_get_db():
 #     async with async_engine.connect() as conn:
@@ -16,5 +15,35 @@ def sync_get_db():
 
 
 def create_tables():
+    sync_engine.echo = False
     metadata_obj.drop_all(sync_engine)
     metadata_obj.create_all(sync_engine)
+    sync_engine.echo = True
+
+
+def insert_data():
+    with sync_engine.connect() as conn:
+        # stmt = """INSERT INTO workers (username) VALUES
+        # ('Bobr'),
+        # ('Wolf');
+        # """
+        stmt = insert(workers_table).values(
+            [
+                {"username": "Bobr"},
+                {"username": "Wolf"},
+            ]
+        )
+        conn.execute(stmt)
+        conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+

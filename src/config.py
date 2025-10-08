c256194b28d74741e7b81cabc5bcea09a564806c
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 11:24:55 +0200

    add path from linux

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent.parent
+ENV_FILE = BASE_DIR / ".env"
+
+if ENV_FILE.exists():
+    load_dotenv(ENV_FILE)
 
 class Settings(BaseSettings):
     DB_HOST: str
@@ -10,7 +17,7 @@ class Settings(BaseSettings):
 
 
     @property
-    def DATABSE_URL_asyncpg(self):
+    def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
@@ -18,7 +25,8 @@ class Settings(BaseSettings):
     def DATABASE_URL_psycopg(self):
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=ENV_FILE,
+                                      env_file_encoding='utf-8')
 
 
 settings = Settings()

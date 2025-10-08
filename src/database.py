e55234219da13987422b8d16f075d3fd235a3762
Slash20 <kurenkov200234@inbox.ru>
2025-10-08 15:48:56 +0200

    del param

--- src/database.py
+++ src/database.py
@@ -8,16 +8,12 @@ from config import settings
 sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
     echo=True,
-    # pool_size=5,
-    # max_overflow=10,
 )
 
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
     echo=True,
-    # pool_size=5,
-    # max_overflow=10,
 )
 
 

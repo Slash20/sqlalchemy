c823f5966c61fce5e116b01da4515fc48609f57e
Slash20 <kurenkov200234@inbox.ru>
2025-10-09 18:24:03 +0200

    add model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,15 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+import datetime
+from typing import Optional
+
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
+import enum
+
+
+class Workload(enum.Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
 
 
 class WorkersORM(Base):
@@ -8,6 +17,17 @@ class WorkersORM(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
+
+
+class ResumeORM(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    compensation: Mapped[Optional[int]]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
 
 
 metadata_obj = MetaData()

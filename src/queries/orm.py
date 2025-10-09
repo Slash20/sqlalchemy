2aee71ccf4c57cdac6751aa33f47b8de9e636f41
Slash20 <kurenkov200234@inbox.ru>
2025-10-09 18:59:52 +0200

    refactoring

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,11 +1,11 @@
-from database import async_engine, sync_engine, session_factory
-from models import metadata_obj, WorkersORM
+from database import async_engine, sync_engine, session_factory, Base
+from models import WorkersORM
 
 
 def create_tables():
-    sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
+    Base.metadata.drop_all(sync_engine)
+    sync_engine.echo = True
+    Base.metadata.create_all(sync_engine)
     sync_engine.echo = True
 
 

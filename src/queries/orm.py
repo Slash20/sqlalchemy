d2d8010f1e6efa971c97b91e32c1b714469308ba
Slash20 <kurenkov200234@inbox.ru>
2025-10-10 19:59:18 +0200

    add class

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -2,16 +2,18 @@ from database import async_engine, sync_engine, session_factory, Base
 from models import WorkersORM
 
 
-def create_tables():
-    Base.metadata.drop_all(sync_engine)
-    sync_engine.echo = True
-    Base.metadata.create_all(sync_engine)
-    sync_engine.echo = True
+class SyncORM:
+    @staticmethod
+    def create_tables():
+        Base.metadata.drop_all(sync_engine)
+        sync_engine.echo = True
+        Base.metadata.create_all(sync_engine)
+        sync_engine.echo = True
 
-
-def insert_data():
-    worker_bobr = WorkersORM(username="Bobr")
-    worker_wolf = WorkersORM(username="Wolf")
-    with session_factory() as session:
-        session.add_all([worker_bobr, worker_wolf])
-        session.commit()
+    @staticmethod
+    def insert_data():
+        worker_bobr = WorkersORM(username="Bobr")
+        worker_wolf = WorkersORM(username="Wolf")
+        with session_factory() as session:
+            session.add_all([worker_bobr, worker_wolf])
+            session.commit()

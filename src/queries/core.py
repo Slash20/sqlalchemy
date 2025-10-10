f5bfd0bc1b0c1f703f14e06a865c08b0c5bdc3fb
Slash20 <kurenkov200234@inbox.ru>
2025-10-10 19:58:38 +0200

    add class

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,28 +1,41 @@
 from database import async_engine, sync_engine
-from sqlalchemy import text, insert
+from sqlalchemy import text, insert, select, update
 from models import metadata_obj, workers_table
 
 
-def create_tables():
-    sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
-    sync_engine.echo = True
+class SyncCore:
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        sync_engine.echo = True
 
+    @staticmethod
+    def insert_data():
+        with sync_engine.connect() as conn:
+            # stmt = """INSERT INTO workers (username) VALUES
+            # ('Bobr'),
+            # ('Wolf');
+            # """
+            stmt = insert(workers_table).values(
+                [
+                    {"username": "Bobr"},
+                    {"username": "Wolf"},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()
 
-def insert_data():
-    with sync_engine.connect() as conn:
-        # stmt = """INSERT INTO workers (username) VALUES
-        # ('Bobr'),
-        # ('Wolf');
-        # """
-        stmt = insert(workers_table).values(
-            [
-                {"username": "Bobr"},
-                {"username": "Wolf"},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()
+    @staticmethod
+    def select_workers():
+        with sync_engine.connect() as conn:
+            query = select(workers_table)
+            result = conn.execute(query)
+            workers = result.all()
+            print(f"{workers=}")
 
-
+    @staticmethod
+    def update_workers(workers_id: int):
+        with sync_engine.connect() as conn:
+            ...

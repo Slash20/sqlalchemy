e0b88d33f82ce596343ca75b3befd14f1ac9956f
Slash20 <kurenkov200234@inbox.ru>
2025-10-16 20:33:15 +0200

    add upd and select

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,5 +1,6 @@
 from database import async_engine, sync_engine, session_factory, Base
 from models import WorkersORM
+from sqlalchemy import select
 
 
 class SyncORM:
@@ -12,8 +13,25 @@ class SyncORM:
 
     @staticmethod
     def insert_data():
-        worker_bobr = WorkersORM(username="Bobr")
-        worker_wolf = WorkersORM(username="Wolf")
         with session_factory() as session:
+            worker_bobr = WorkersORM(username="Jack")
+            worker_wolf = WorkersORM(username="Wolf")
             session.add_all([worker_bobr, worker_wolf])
+            session.commit()
+
+    @staticmethod
+    def select_workers():
+        with session_factory() as session:
+            # worker_id = 1
+            # worker_jack = session.get(WorkersORM, worker_id)
+            query = select(WorkersORM)
+            result = session.execute(query)
+            workers = result.scalars().all()
+            print(f"{workers=}")
+
+    @staticmethod
+    def update_workers(worker_id: int = 2, new_username: str = "Misha"):
+        with session_factory() as session:
+            worker_michael = session.get(WorkersORM, worker_id)
+            worker_michael.username = new_username
             session.commit()

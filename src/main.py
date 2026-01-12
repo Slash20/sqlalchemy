3a186a7e9c720b3691d64e4cb14792c203ae797e
Slash20 <kurenkov200234@inbox.ru>
2026-01-12 19:27:11 +0200

    add insert

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from queries.core import SyncCore
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 SyncORM.create_tables()
-SyncORM.insert_data()
+SyncORM.insert_workers()
+SyncORM.insert_resumes()
 # SyncCore.select_workers()
 # SyncCore.update_workers()
 SyncORM.select_workers()

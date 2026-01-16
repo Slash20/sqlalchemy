4ff438d84b25eb01ef6b2a79fb9bb4ea9bfc1659
Slash20 <kurenkov200234@inbox.ru>
2026-01-16 20:14:15 +0200

    add run fastapi

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-import os
-import sys
 from queries.orm import SyncORM
-
-# sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 SyncORM.create_tables()
 SyncORM.insert_workers()
@@ -14,3 +12,21 @@ SyncORM.select_workers_lazy_relationship()
 SyncORM.select_workers_joined_relationship()
 SyncORM.select_workers_selectin_relationship()
 SyncORM.select_workers_with_condition_relationship()
+
+def create_fastapi_app():
+
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+    )
+
+    @app.get("/workers")
+    async def get_workers():
+        workers = SyncORM.convert_workers_to_dto()
+        return workers
+
+    return app
+
+app = create_fastapi_app()
